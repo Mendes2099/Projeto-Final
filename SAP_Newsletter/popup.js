f16ddd3a83d61ed8633a1da19a535c8c84c63249
João Mendes <104670865+Mendes2099@users.noreply.github.com>
2023-06-22 18:07:33 +0100

    There ya go.

--- SAP_Newsletter/popup.js
+++ SAP_Newsletter/popup.js
@@ -41,24 +41,35 @@ function createEML(jobLinks, jobTitles, jobLocations) {
 
   // Replace placeholders in the template with the job links, titles, and categories
   const emailBody = jobLinks.map((link, index) => {
+    const jobTitlesStyle = 'font-size: 13.3333px; color: #000000; text-decoration: none solid rgb(0, 0, 0); font-family: Arial, sans-serif;';
+    const jobLocationStyle = 'font-size: 10.6667px; color: #A5A5A5; text-decoration: none solid rgb(165, 165, 165); font-family: Arial, sans-serif;';
     const jobLinkHtml = `
-      <p>
-        <a href="https://${domain}${link}">Internal link</a><br>
-        <span>${jobTitles[index]}</span><br>
-        <span>${jobLocations[index]}</span>
-      </p>
+    <p style="font-size: 13.3333px; color: #0563C1; text-decoration: none solid rgb(5, 99, 193); font-family: Arial, sans-serif;">
+      <strong><span style="margin-right: 5px;">&#9642;</span> <span style="${jobTitlesStyle}">${jobTitles[index]}</span> - 
+      <span style="${jobLocationStyle}">${jobLocations[index]}</span> - 
+      <a href="https://${domain}${link}">Internal link</a>
+    </p>
     `;
     return jobLinkHtml;
   }).join('');
 
   const htmlTemplate = `
-    <html>
+  <!DOCTYPE html>
+  <html>
+    <head>
+    <style>
+    .positions {
+      font-size: 17px;
+      color: #008FD3;
+    }
+    </style>
+    </head>
     <body>
-      <p>Job Links:</p>
+    <p class="positions">(INTERNAL / EXTERNAL) POSITIONS:</p>
       ${emailBody}
     </body>
-    </html>
-  `;
+  </html>
+`;
 
   const emlData = `From: me@example.com
 To: you@example.com
